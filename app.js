525ac6263789e3e88eed45e0156aaaf54e8284e2
Latifa <latifaabdullayeva@gmail.com>
2021-02-14 18:07:50 +0400

    Adds event to the form button

---
+++ app.js
@@ -0,0 +1,19 @@
+// Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+// Event Listeners
+todoButton.addEventListener("click", addTodo);
+
+// Functions
+function addTodo(event) {
+    // Prevent form from submitting
+  event.preventDefault();
+  console.log("hello");
+}
+
+/*
+- .addEventListener("click", someFunc); // more than click
+- event.preventDefault();
+*/
